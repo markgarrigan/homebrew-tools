f73a9616af08e839e4c3d2e97e05c2087d223f83
Mark Garrigan <mark@markgarrigan.com>
2020-05-07 00:43:28 -0500

    updates version and sha

--- Formula/bake.rb
+++ Formula/bake.rb
@@ -1,8 +1,8 @@
 class Bake < Formula
   desc "Baking web development environments."
   homepage "https://github.com/markgarrigan/bake"
-  url "https://github.com/markgarrigan/bake/archive/v0.0.4.tar.gz"
-  sha256 "b4086907db256f9006d0ef6d1f8a51bb0745705dce2a2a1e11667b51d8a6ae3e"
+  url "https://github.com/markgarrigan/bake/archive/v0.0.5.tar.gz"
+  sha256 "0940376c4885be515bf685e09dc4cec33167d203d446c647237f9ffd1e221b2a"
 
   def install
     bin.install "bake"
